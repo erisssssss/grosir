213b2cabf51a06b0763aca14490afeee45e25070
erisssssss <168049674+erisssssss@users.noreply.github.com>
2024-05-22 15:46:33 +0700

    update halaman dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,11 @@
 @extends('layouts.master')
 @section('title','Dashboard')
 @section('heading','Halaman Dashboard')
+
+@section('content')
+        <div class="card-body">
+        Halo {{ Auth::user()->name }}, <br> 
+        </div>
+        <!-- /.card-body -->
+
+@endsection
